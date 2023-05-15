7de109fe742fbc89f461cb418afdf483ec652a8f
ToDownTeam <todownllc@gmail.com>
2023-05-15 18:33:40 +0400

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div className="w-full h-screen flex flex-cols text-center">
+        <div className="grid grid-cols-1 gap-3 w-1/2 mx-auto h-1/2">
+          <div className="text-4xl pt-10">HELLO WORLD</div>
+          <div className="block">
+            <button className="bg-slate-100 shadow-md px-4 py-3"> Get started</button></div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// @ts-ignore
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
